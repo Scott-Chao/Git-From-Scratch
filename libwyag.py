c4098417891eaf71a76bc69d43916326b2ea413f
Scott Chao <zws061003@gmail.com>
2026-03-01 22:57:14 +0800

    feat: implement GitBlob class, cat-file command and hash-object command

--- libwyag.py
+++ libwyag.py
@@ -220,6 +220,16 @@ def object_write(obj, repo=None):
     return sha
 
 
+class GitBlob(GitObject):
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+
 argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
@@ -232,6 +242,36 @@ argsp.add_argument(
     default=".",
     help="Where to create the repository.",
 )
+
+argsp = argsubparsers.add_parser(
+    "cat-file", help="Provide content of repository objects"
+)
+argsp.add_argument(
+    "type",
+    metavar="type",
+    choices=["blob", "commit", "tag", "tree"],
+    help="Specify the type",
+)
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+argsp = argsubparsers.add_parser(
+    "hash-object", help="Compute object ID and optionally creates a blob from a file"
+)
+argsp.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default="blob",
+    help="Specify the type",
+)
+argsp.add_argument(
+    "-w",
+    dest="write",
+    action="store_true",
+    help="Actually write the object into the database",
+)
+argsp.add_argument("path", help="Read object from <file>")
 
 
 def main(argv=sys.argv[1:]):
@@ -276,7 +316,17 @@ def cmd_add(args):
 
 
 def cmd_cat_file(args):
-    pass
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
 
 
 def cmd_check_ignore(args):
@@ -292,7 +342,33 @@ def cmd_commit(args):
 
 
 def cmd_hash_object(args):
-    pass
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
+def object_hash(fd, fmt, repo=None):
+    """Hash object, writing it to repo if provided"""
+    data = fd.read()
+
+    match fmt:
+        case b"commit":
+            obj = GitCommit(data)
+        case b"tree":
+            obj = GitTree(data)
+        case b"tag":
+            obj = GitTag(data)
+        case b"blob":
+            obj = GitBlob(data)
+        case _:
+            raise Exception(f"Unknown type {fmt}!")
+
+    return object_write(obj, repo)
 
 
 def cmd_init(args):
