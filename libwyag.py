c295774919f81401f0a2f1845fde0502ad06f133
Scott Chao <zws061003@gmail.com>
2026-03-02 09:28:53 +0800

    feat: implement log command

--- libwyag.py
+++ libwyag.py
@@ -337,6 +337,9 @@ argsp.add_argument(
 )
 argsp.add_argument("path", help="Read object from <file>")
 
+argsp = argsubparsers.add_parser("log", help="Display history of a given commit.")
+argsp.add_argument("commit", default="HEAD", nargs="?", help="Commit to start at.")
+
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
@@ -440,7 +443,42 @@ def cmd_init(args):
 
 
 def cmd_log(args):
-    pass
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    print("  node[shape=rect]")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+
+def log_graphviz(repo, sha, seen):
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    message = commit.kvlm[None].decode("utf8").strip()
+    message = message.replace("\\", "\\\\")
+    message = message.replace('"', '\\"')
+
+    if "\n" in message:
+        message = message[: message.index("\n")]
+
+    print(f'  c_{sha} [label="{sha[0:7]}: {message}"]')
+    assert commit.fmt == b"commit"
+
+    if not b"parent" in commit.kvlm.keys():
+        return
+
+    parents = commit.kvlm[b"parent"]
+
+    if type(parents) != list:
+        parents = [parents]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print(f"  c_{sha} -> c_{p};")
+        log_graphviz(repo, p, seen)
 
 
 def cmd_ls_files(args):
