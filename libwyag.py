5feb265154f182212bcce14c475eaccd5e62f83c
Scott Chao <zws061003@gmail.com>
2026-03-03 11:56:44 +0800

    feat: implement ls-files command

--- libwyag.py
+++ libwyag.py
@@ -855,6 +855,9 @@ argsp.add_argument(
 )
 argsp.add_argument("name", help="The name to parse")
 
+argsp = argsubparsers.add_parser("ls-files", help="List all the stage files")
+argsp.add_argument("--verbose", action="store_true", help="Show everything.")
+
 
 def main(argv: Optional[List[str]] = None) -> None:
     if argv is None:
@@ -980,7 +983,33 @@ def cmd_log(args: argparse.Namespace) -> None:
 
 
 def cmd_ls_files(args: argparse.Namespace) -> None:
-    pass
+    repo = GitRepository.find()
+    index = index_read(repo)
+    if args.verbose:
+        print(
+            f"Index file format v{index.version}, containing {len(index.entries)} entries."
+        )
+
+    for e in index.entries:
+        print(e.name)
+        if args.verbose:
+            entry_type = {
+                0b1000: "regular_file",
+                0b1010: "symlink",
+                0b1110: "git link",
+            }[e.mode_type]
+            print(f"  {entry_type} with perms: {e.mode_perms:o}")
+            print(f"  on blob: {e.sha}")
+            print(
+                f"  created: {datetime.fromtimestamp(e.ctime[0])}.{e.ctime[1]}, modified: {datetime.fromtimestamp(e.mtime[0])}.{e.mtime[1]}"
+            )
+            print(f"  device: {e.dev}, inode: {e.ino}")
+            try:
+                print(
+                    f"  user: {pwd.getpwuid(e.uid).pw_name} ({e.uid})  group: {grp.getgrgid(e.gid).gr_name} ({e.gid})"
+                )
+            except NameError:
+                print(f"  user: {e.uid}  group: {e.gid}")
 
 
 def cmd_ls_tree(args: argparse.Namespace) -> None:
