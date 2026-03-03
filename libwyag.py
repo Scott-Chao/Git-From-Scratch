514f474f5f270b67733cfaca6076e55bc6061cbe
Scott Chao <zws061003@gmail.com>
2026-03-03 13:02:53 +0800

    feat: implement check-ignore command

--- libwyag.py
+++ libwyag.py
@@ -769,6 +769,110 @@ def index_read(repo):
     return GitIndex(version=version, entries=entries)
 
 
+class GitIgnore(object):
+    absolute = None
+    scoped = None
+
+    def __init__(self, absolute, scoped):
+        self.absolute = absolute
+        self.scoped = scoped
+
+
+def gitignore_parse_one(raw):
+    raw = raw.strip()
+
+    if not raw or raw[0] == "#":
+        return None
+    if raw[0] == "!":
+        return (raw[1:], False)
+    if raw[0] == "\\":
+        return (raw[1:], True)
+    return (raw, True)
+
+
+def gitignore_parse(lines):
+    ret = list()
+
+    for line in lines:
+        parsed = gitignore_parse_one(line)
+        if parsed:
+            ret.append(parsed)
+
+    return ret
+
+
+def gitignore_read(repo):
+    ret = GitIgnore(absolute=list(), scoped=dict())
+
+    repo_file = os.path.join(repo.gitdir, "info/exclude")
+    if os.path.exists(repo_file):
+        with open(repo_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    if "XDG_CONFIG_HOME" in os.environ:
+        config_home = os.environ["XDG_CONFIG_HOME"]
+    else:
+        config_home = os.path.expanduser("~/.config")
+    global_file = os.path.join(config_home, "git/ignore")
+
+    if os.path.exists(global_file):
+        with open(global_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    index = index_read(repo)
+    for entry in index.entries:
+        if entry.name == ".gitignore" or entry.name.endswith("/.gitignore"):
+            dir_name = os.path.dirname(entry.name)
+            contents = repo.read_object(entry.sha)
+            lines = contents.blobdata.decode("utf8").splitlines()
+            ret.scoped[dir_name] = gitignore_parse(lines)
+
+    return ret
+
+
+def check_ignore_one(rules, path):
+    result = None
+    for pattern, value in rules:
+        if fnmatch(path, pattern):
+            result = value
+    return result
+
+
+def check_ignore_scoped(rules, path):
+    parent = os.path.dirname(path)
+    while True:
+        if parent in rules:
+            result = check_ignore_one(rules[parent], path)
+            if result != None:
+                return result
+        if parent == "":
+            break
+        parent = os.path.dirname(parent)
+    return None
+
+
+def check_ignore_absolute(rules, path):
+    parent = os.path.dirname(path)
+    for ruleset in rules:
+        result = check_ignore_one(ruleset, path)
+        if result != None:
+            return result
+    return False
+
+
+def check_ignore(rules, path):
+    if os.path.isabs(path):
+        raise Exception(
+            "This function requires path to be relative to the repository's root"
+        )
+
+    result = check_ignore_scoped(rules.scoped, path)
+    if result != None:
+        return result
+
+    return check_ignore_absolute(rules.absolute, path)
+
+
 # CLI definition
 argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
@@ -858,6 +962,11 @@ argsp.add_argument("name", help="The name to parse")
 argsp = argsubparsers.add_parser("ls-files", help="List all the stage files")
 argsp.add_argument("--verbose", action="store_true", help="Show everything.")
 
+argsp = argsubparsers.add_parser(
+    "check-ignore", help="Check path(s) against ignore rules."
+)
+argsp.add_argument("path", nargs="+", help="Paths to check")
+
 
 def main(argv: Optional[List[str]] = None) -> None:
     if argv is None:
@@ -915,7 +1024,11 @@ def cmd_cat_file(args: argparse.Namespace) -> None:
 
 
 def cmd_check_ignore(args: argparse.Namespace) -> None:
-    pass
+    repo = GitRepository.find()
+    rules = gitignore_read(repo)
+    for path in args.path:
+        if check_ignore(rules, path):
+            print(path)
 
 
 def cmd_checkout(args: argparse.Namespace) -> None:
